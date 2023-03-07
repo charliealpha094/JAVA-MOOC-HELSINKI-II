831fcfa899e4543f85b5f96a46852c2c1dfdf709
charliealpha094 <carlosamaral94@gmail.com>
2023-03-07 18:38:05 +0000

    Exercise 06- Part 8 - Nicknames

---
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -0,0 +1,14 @@
+
+import java.util.HashMap;
+
+public class Nicknames {
+
+    public static void main(String[] args) {
+        HashMap<String, String> nickNames = new HashMap<>();
+        
+        nickNames.put("matthew", "matt");
+        nickNames.put("michael", "mix");
+        nickNames.put("arthur", "artie");
+    }
+
+}
