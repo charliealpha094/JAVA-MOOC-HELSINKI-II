ec77852aedc24717ec63eedf0263dad91dd8dada
charliealpha094 <carlosamaral94@gmail.com>
2023-03-21 21:21:38 +0000

    Exercises 07-08 part8

---
+++ part08-Part08_07.Abbreviations/src/main/java/Abbreviations.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author carlos
+ */
+import java.util.HashMap;
+
+public class Abbreviations {
+    
+    private HashMap<String, String> abbreviations;
+    
+    // Constructor
+    public Abbreviations() {
+        this.abbreviations = new HashMap<>();
+    }
+    
+    public void addAbbreviation(String abbreviation, String explanation) {
+        this.abbreviations.put(abbreviation, explanation);
+    }
+    
+    public boolean hasAbbreviation(String abbreviation) {
+        if(this.abbreviations.containsKey(abbreviation)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public String findExplanationFor(String abbreviation) {
+        if(this.hasAbbreviation(abbreviation)) {
+            return this.abbreviations.get(abbreviation);
+        } else {
+            return null;
+        }
+    }
+}
